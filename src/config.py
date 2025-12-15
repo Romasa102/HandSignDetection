424855a4a6486eac24610439072613a14b6792bb
Romasa102 <masa.robot123@gmail.com>
2025-12-15 02:36:20 +0000

    added some comments

--- src/config.py
+++ src/config.py
@@ -12,17 +12,17 @@ SEQUENCE_LENGTH = 30 #How long the video would be (frame count)
 
 #Training setting
 
-TRAINING_DATAFILE_NAME = 'MP_Data' #Set your file's name here
+TRAINING_DATAFILE_NAME = 'MP_Data' #Which dataset to use for training
 
-OPTIMIZER = 'Adam'
+OPTIMIZER = 'Adam' #Optimizer to use
 
-L2NORM = ''
+L2NORM = '' #L2 norm regularization term
 
-EPOCHS = 2000
+EPOCHS = 2000 #Epoch count
 
 MODEL_SAVE_DIRECTORY = 'models'
 
 #Model
 
-MODEL_DIRECTORY = 'models/action.h5'
+MODEL_DIRECTORY = 'models/action.h5' #The model you would like to use for the test
 
